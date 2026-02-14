f8bebbb3bff9b0d67d3405643b1a4b7ceb24c0d2
Lucky_joy_228 <arseniibykadorov175@gmail.com>
2026-02-14 21:39:59 +0200

    Added buttn, buzzer, LCD

---
+++ arduinotest/src/main.cpp
@@ -0,0 +1,62 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+#define button 8
+int button_state = 0;
+int buzzerPin = 5; //Define buzzerPin
+LiquidCrystal_I2C lcd(0x27, 16, 2); // адреса, колонки, рядки
+
+
+void beep(unsigned char delayms) { //creating function
+  analogWrite(buzzerPin, 20); //Setting pin to high
+  delay(delayms); //Delaying
+  analogWrite(buzzerPin ,0); //Setting pin to LOW
+  delay(delayms); //Delaying
+  
+}
+
+
+void setup()
+{
+
+  pinMode(button, INPUT);
+  Serial.begin(9600); // open the serial port at 9600 bps:
+
+  // PWM setup
+    pinMode(buzzerPin, OUTPUT); //Set buzzerPin as output
+  beep(100); //Beep
+  beep(30); //Beep
+
+  // setupLCD
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);       // колонка 0, рядок 0
+  lcd.print("display test"); // новий текст
+  delay(2000);
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+  button_state = digitalRead(button); // read the input pin
+  // Serial.print("button state: ");     // prints a label
+  // Serial.print(button_state);         // prints a label
+  // Serial.print("\n");                 // prints a label
+  // delay(1000);
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  // lcd.clear();dff
+  // lcd.setCursor(5, 0);
+  if (button_state == 0)
+  {
+    // lcd.print("ON"); // новий текст
+    // Serial.print("ON\n"); 
+    beep(100); //Beep
+  }
+  else
+  {
+    // lcd.print("OFF"); // новий текст
+    // Serial.print("OFF\n"); 
+  }
+}
